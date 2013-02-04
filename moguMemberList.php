9281380707f3e1ccb7e4068a8562f5255a18eda4
curious-eyes <curious.staff@gmail.com>
2013-02-05 02:46:20 +0900

    Style修正

--- moguMemberList.php
+++ moguMemberList.php
@@ -8,7 +8,8 @@ get_header(); ?>
 <style type='text/css'><!--
     .member {
         width: 98%;
-        padding-top: 25px;
+        margin-top: 24px;
+        padding-top: 24px;
         border-top: solid 1px #e3e3e3;
     }
 
@@ -16,8 +17,14 @@ get_header(); ?>
         line-height: 24px;
         margin-bottom: 24px;
     }
+    .member p.avatar{
+        display:inline;
+        float:right;
+        margin-left:5px;
+    }
     .member p.fullname {
         font-size: x-large;
+        margin-top: 16px;
     }
     .member img.social{
         width: 48px;
@@ -43,22 +50,22 @@ get_header(); ?>
             $facebook = $user->facebook;
             $twitter = $user->twitter;
             $google = $user->google;
-            echo '<p style="display:inline;float:right;">'.get_avatar($user->ID, 150).'</p>';
+            echo '<p class="avatar">'.get_avatar($user->ID, 150).'</p>';
+            if (strlen($google) > 0){
+                echo '<span style="float:right;"><a href="http://plus.google.com/u/0/'.$google.'" target="_blank"><img class="social" src="'.$pathToTemplate.'/images/social/Google plus.png" alt="googleplus" /></a></span>';
+            }
+            if (strlen($facebook) > 0){
+                echo '<span style="float:right;"><a href="http://www.facebook.com/'.$facebook.'" target="_blank"><img class="social" src="'.$pathToTemplate.'/images/social/Facebook.png" alt="facebook" /></a></span>';
+            }
+            if (strlen($twitter) > 0){
+                echo '<span style="float:right;"><a href="http://www.twitter.com/'.$twitter.'" target="_blank"><img src="'.$pathToTemplate.'/images/social/Twitter.png" alt="twitter" /></a></span>';
+            }
             echo '<p class="fullname"><strong>'.$user->last_name." ".$user->first_name.'</strong></p>';
             echo '<p>'.nl2br($user->user_description).'</p>';
             if (intval($page_id) > 0){
                 echo '<p><strong><a href="'.$pathToRoot.'/?page_id='.$page_id.'">自己紹介ページへ</strong></p></a><br />';
             }
-            if (strlen($facebook) > 0){
-                echo '<a href="http://www.facebook.com/'.$facebook.'" target="_blank"><img class="social" src="'.$pathToTemplate.'/images/social/Facebook.png" alt="facebook" /></a>';
-            }
-            if (strlen($twitter) > 0){
-                echo '<a href="http://www.twitter.com/'.$twitter.'" target="_blank"><img src="'.$pathToTemplate.'/images/social/Twitter.png" alt="twitter" /></a>';
-            }
-            if (strlen($google) > 0){
-                echo '<a href="http://plus.google.com/u/0/'.$google.'" target="_blank"><img class="social" src="'.$pathToTemplate.'/images/social/Google plus.png" alt="googleplus" /></a>';
-            }
-            echo "</div>";
+            echo '</div>';
             ?>
         <?php endforeach; ?>
 
